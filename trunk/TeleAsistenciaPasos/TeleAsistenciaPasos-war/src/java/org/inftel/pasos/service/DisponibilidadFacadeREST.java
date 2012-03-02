0d71a860d7027f822e94f6097f598dc329dc9e7b
ajrneo86@gmail.com <ajrneo86@gmail.com@123e7113-6f54-7395-d328-dbae6a2abf9b>
2012-03-02 12:19:52 +0000

    Creada funcionalidad REST

---
+++ trunk/TeleAsistenciaPasos/TeleAsistenciaPasos-war/src/java/org/inftel/pasos/service/DisponibilidadFacadeREST.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inftel.pasos.service;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.*;
+import org.inftel.pasos.entity.Disponibilidad;
+
+/**
+ *
+ * @author aljiru
+ */
+@Stateless
+@Path("org.inftel.pasos.tables.disponibilidad")
+public class DisponibilidadFacadeREST extends AbstractFacade<Disponibilidad> {
+    @PersistenceContext(unitName = "TeleAsistenciaPasos-warPU")
+    private EntityManager em;
+
+    public DisponibilidadFacadeREST() {
+        super(Disponibilidad.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Disponibilidad entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void edit(Disponibilidad entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Disponibilidad find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Disponibilidad> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Disponibilidad> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @java.lang.Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
---
+++ trunk/TeleAsistenciaPasos/TeleAsistenciaPasos-war/src/java/org/inftel/pasos/service/TurnoFacadeREST.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.inftel.pasos.service;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.*;
+import org.inftel.pasos.entity.Turno;
+
+
+/**
+ *
+ * @author aljiru
+ */
+@Stateless
+@Path("org.inftel.pasos.tables.turno")
+public class TurnoFacadeREST extends AbstractFacade<Turno> {
+    @PersistenceContext(unitName = "TeleAsistenciaPasos-warPU")
+    private EntityManager em;
+
+    public TurnoFacadeREST() {
+        super(Turno.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(Turno entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void edit(Turno entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Long id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public Turno find(@PathParam("id") Long id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<Turno> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<Turno> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @java.lang.Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
